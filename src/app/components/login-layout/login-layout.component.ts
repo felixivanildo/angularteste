74c804b52525e882a42ab014a648e3380ddfcb53
Ivan Felix <felixsilva104@gmail.com>
2024-09-23 09:32:33 -0300

    feat: login form style done

---
+++ src/app/components/login-layout/login-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-login-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-layout.component.html',
+  styleUrl: './login-layout.component.css'
+})
+export class LoginLayoutComponent {
+  @Input() title: string = "";
+  @Input() primaryBtnText: string = "";
+  @Input() secondaryBtnText: string = "";
+  
+}
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { LoginLayoutComponent } from '../../components/login-layout/login-layout.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [LoginLayoutComponent, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  loginForm!: FormGroup;
+
+  constructor(){
+    this.loginForm = new FormGroup({
+        email:  new FormControl('', [Validators.required]),
+        password:  new FormControl('', [Validators.required])
+    })
+  }
+}
